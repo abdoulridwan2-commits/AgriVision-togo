62ff8c4f64a566ed2f6ad72e38b98f746a0c9190
abdoulridwan2-commits <abdoulridwan2@gmail.com>
2026-06-21 20:24:05 +0000

    deuxième commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,63 @@
 import { useEffect, useState } from "react";
 
 function Dashboard() {
-  const [totalCooperatives, setTotalCooperatives] = useState(0);
+  const [stats, setStats] = useState({
+    cooperatives: 0,
+    grandes: 0,
+    petites: 0,
+    plantations: 0,
+    zaaps: 0,
+    pepinieres: 0,
+  });
+
+  const loadData = async (fileName, key) => {
+    try {
+      const response = await fetch(`/data/${fileName}`);
+      const data = await response.json();
+
+      console.log(fileName, data);
+
+      let total = 0;
+
+      if (data.features) {
+        total = data.features.length;
+      } else if (Array.isArray(data)) {
+        total = data.length;
+      }
+
+      console.log(`${fileName} => ${total}`);
+
+      setStats((prev) => ({
+        ...prev,
+        [key]: total,
+      }));
+    } catch (error) {
+      console.error(`Erreur avec ${fileName}`, error);
+    }
+  };
 
   useEffect(() => {
-    fetch("/data/Cooperatives.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setTotalCooperatives(data.features.length);
-        console.log(data);
-      })
-      .catch((error) => console.error(error));
+    loadData("cooperatives.json", "cooperatives");
+    loadData("grandes_exploitations.json", "grandes");
+    loadData("petites_exploitations.json", "petites");
+    loadData("plantations_agricoles.json", "plantations");
+    loadData("zaaps_champs.json", "zaaps");
+    loadData("pepinieres_agricoles.json", "pepinieres");
   }, []);
 
   return (
-    <div>
-      <h1>AgriVision Togo</h1>
+    <div style={{ padding: "20px" }}>
+      <h1> AgriVision Togo</h1>
       <p>Tableau de bord agricole du Togo</p>
 
-      <h2>Coopératives Agricoles</h2>
-      <p>Total : {totalCooperatives}</p>
+      <h2>📊 Statistiques Générales</h2>
+
+      <p> Coopératives : {stats.cooperatives}</p>
+      <p> Grandes exploitations : {stats.grandes}</p>
+      <p> Petites exploitations : {stats.petites}</p>
+      <p> Plantations : {stats.plantations}</p>
+      <p> ZAAPs / ZAPBs : {stats.zaaps}</p>
+      <p> Pépinières : {stats.pepinieres}</p>
     </div>
   );
 }
